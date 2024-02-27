22c7c1a9fe6956303a1d5e3ee74bf4888c0bdeb6
stella <khk9562@naver.com>
2024-02-27 23:14:19 +0900

    redux 설치 및 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import List from "./components/List";
+import { Provider as MyProvider } from "react-redux";
+// import { configureStore } from "@reduxjs/toolkit";
 
 function App() {
+  // const store = configureStore({ reducer: r });
+
   return (
+    // <MyProvider store={store}>
     <main>
       <List />
     </main>
+    // </MyProvider>
   );
 }
 
